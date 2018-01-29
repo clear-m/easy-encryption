b65989f2d2e05c6968d9534cc7898d6389de56ff
Alex Kononov <a.kononov@myfxchoice.com>
2018-01-29 15:49:44 +0300

    namespace isolation added

--- encrypt.h
+++ encrypt.h
@@ -1,17 +1,29 @@
+#pragma once
+
+#ifndef _ENCRYPT_
+#define _ENCRYPT_
+
 #include "b64.h"
 #include "vigenere.h"
 
-std::string encrypt(std::string& msg, std::string& key) {
-	std::string b64_str = base64_encode(msg);
-	std::string vigenere_msg = encrypt_vigenere(b64_str, key);
-	// std::cout << vigenere_msg << std::endl;
-	return vigenere_msg;
-}
+namespace encrypt
+{
+	std::string encrypt(std::string& msg, std::string& key)
+	{
+		std::string b64_str = base64_encode(msg);
+		std::string vigenere_msg = encrypt_vigenere(b64_str, key);
+		// std::cout << vigenere_msg << std::endl;
+		return vigenere_msg;
+	}
 
 
-std::string decrypt(std::string& encrypted_msg, std::string& key) {
-	std::string newKey = extend_key(encrypted_msg, key);
-	std::string b64_encoded_str = decrypt_vigenere(encrypted_msg, newKey);
-	std::string b64_decode_str = base64_decode(b64_encoded_str);
-	return b64_decode_str;
+	std::string decrypt(std::string& encrypted_msg, std::string& key)
+	{
+		std::string newKey = extend_key(encrypted_msg, key);
+		std::string b64_encoded_str = decrypt_vigenere(encrypted_msg, newKey);
+		std::string b64_decode_str = base64_decode(b64_encoded_str);
+		return b64_decode_str;
+	}
 }
+
+#endif
